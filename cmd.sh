7b87be75b7e46688d6d7ccee74cf6029466ba497
Rahul Yadav <56602737+rahulOCR@users.noreply.github.com>
2023-12-27 12:55:06 +0530

    Update cmd.sh for base_port

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+# Base TCP port for sitl <-> GCS connection
+BASEPORT=4444
 # Command to run SITL
-command1="./arduplane -S --model quadplane --speedup 1 --slave 0 --defaults quadplane.parm -I0 --home 63.98504,-22.654108000000008,30.0,89.0"
+command1="./arduplane -S --model quadplane --speedup 1 --slave 0 --defaults quadplane.parm -I0 --base-port ${BASEPORT} --home 63.98504,-22.654108000000008,30.0,89.0"
 
 # Command to run GCS mavproxy
-command2="mavproxy.py --master=tcp:127.0.0.1:5760 --out=udp::14553"
+command2="mavproxy.py --master=tcp:127.0.0.1:${BASEPORT} --out=udp::14553"
 
 # Open the SITL tab
 gnome-terminal --tab --title="SITL" -- bash -c "$command1"
