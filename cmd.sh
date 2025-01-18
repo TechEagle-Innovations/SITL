0771924ee4d251d55bfae16dbe8ea205cb9a066a
Rahul <rahulec1999@gmail.com>
2025-01-18 21:38:59 +0530

    converted tcp to udp and sitl bug fix

--- cmd.sh
+++ cmd.sh
@@ -2,15 +2,15 @@
 # Base TCP port for sitl <-> GCS connection
 BASEPORT=4444
 # Command to run SITL
-command1="./arduplane -S --model quadplane --speedup 1 --slave 0 --defaults quadplane.parm -I0 --base-port ${BASEPORT} --home 63.98504,-22.654108000000008,30.0,89.0"
+command1="./arduplane -S --model quadplane --speedup 1 --slave 0 --defaults quadplane.parm -I0 --serial0=udpclient:0.0.0.0:${BASEPORT} --home 63.98504,-22.654108000000008,30.0,89.0"
 
 # Command to run GCS mavproxy
-command2="mavproxy.py --master=tcp:127.0.0.1:${BASEPORT} --out=udp::14553"
+command2="mavproxy.py --master=udp:127.0.0.1:${BASEPORT} --out=udp::14553"
 
 # Open the SITL tab
 gnome-terminal --tab --title="SITL" -- bash -c "$command1"
 
 # Open the GCS tab
-gnome-terminal --tab --title="GCS" -- bash -c "$command2"
+# gnome-terminal --tab --title="GCS" -- bash -c "$command2"
 
 
